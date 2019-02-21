1e6d3488d5cce5532a7b5197ffd75701d44b8df9
Rommel <cunetarommel0620@gmail.com>
2019-02-21 08:42:33 +0800

    setup

---
+++ src/main.js
@@ -0,0 +1,26 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+
+import './styles/quasar.styl'
+import 'quasar-framework/dist/quasar.ie.polyfills'
+import 'quasar-extras/animate'
+import 'quasar-extras/roboto-font'
+import 'quasar-extras/material-icons'
+import 'quasar-extras/fontawesome'
+import 'quasar-extras/ionicons'
+import 'quasar-extras/mdi'
+import Quasar from 'quasar'
+
+Vue.use(Quasar, {
+  config: {}
+ })
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
